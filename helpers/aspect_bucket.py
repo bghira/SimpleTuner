88eb08801ad805a18baad1c521a493b11113d0af
bghira <bghira@users.github.com>
2023-06-17 16:10:37 -0700

    Aspect ratio bucketing, WIP

---
+++ helpers/aspect_bucket.py
@@ -0,0 +1,22 @@
+import torch
+
+class BalancedBucketSampler(torch.utils.data.Sampler):
+    def __init__(self, aspect_ratio_bucket_indices, batch_size):
+        self.aspect_ratio_bucket_indices = aspect_ratio_bucket_indices
+        self.buckets = list(aspect_ratio_bucket_indices.keys())
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        while True:
+            for bucket in self.buckets:
+                batch_indices = []
+                for _ in range(self.batch_size):
+                    if self.aspect_ratio_bucket_indices[bucket]:
+                        batch_indices.append(self.aspect_ratio_bucket_indices[bucket].pop())
+                    else:
+                        break
+                if len(batch_indices) == self.batch_size:  # Only yield complete batches
+                    yield batch_indices
+
+    def __len__(self):
+        return sum(len(indices) for indices in self.aspect_ratio_bucket_indices.values())
