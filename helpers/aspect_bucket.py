ee3fa8c06e60fdf25beb7b10e58e825c49935803
bghira <bghira@users.github.com>
2023-06-17 20:52:42 -0700

    Add workaround for empty bucket

--- helpers/aspect_bucket.py
+++ helpers/aspect_bucket.py
@@ -1,4 +1,4 @@
-import torch
+import torch, time
 
 class BalancedBucketSampler(torch.utils.data.Sampler):
     def __init__(self, aspect_ratio_bucket_indices, batch_size=15):
@@ -24,6 +24,8 @@ class BalancedBucketSampler(torch.utils.data.Sampler):
             else:
                 print(f'Bucket {bucket} is empty. Moving to the next bucket.')
                 self.current_bucket = (self.current_bucket + 1) % len(self.buckets)
+                # Wait a second.
+                time.sleep(10)
 
     def __len__(self):
         return sum(len(indices) for indices in self.aspect_ratio_bucket_indices.values())
