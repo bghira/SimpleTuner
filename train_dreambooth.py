a925b1aea6619770796e331b0e66cb1960be44bb
bghira <bghira@users.github.com>
2023-06-18 11:50:14 -0700

    Trigger the dataset when we need it

--- train_dreambooth.py
+++ train_dreambooth.py
@@ -77,6 +77,7 @@ to_tensor = ToTensor()
 
 def collate_fn(examples):
     if not StateTracker.status_training():
+        logging.debug('collate_fn: not training, returning examples.')
         return examples
 
     input_ids = [example["instance_prompt_ids"] for example in examples]
