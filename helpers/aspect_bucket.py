1e7b5c0a060b8da2f8d31a611e37244b2558dd23
bghira <bghira@users.github.com>
2023-06-17 18:44:30 -0700

    The current bucket should be stored and iterated from until exhaustion in the current batch.

--- helpers/aspect_bucket.py
+++ helpers/aspect_bucket.py
@@ -1,19 +1,26 @@
 import torch
 
 class BalancedBucketSampler(torch.utils.data.Sampler):
-    def __init__(self, aspect_ratio_bucket_indices, batch_size = 15):
+    def __init__(self, aspect_ratio_bucket_indices, batch_size=15):
         self.aspect_ratio_bucket_indices = aspect_ratio_bucket_indices
         self.buckets = list(aspect_ratio_bucket_indices.keys())
         self.batch_size = batch_size
+        self.current_bucket = 0
+        self.current_count = 0
 
     def __iter__(self):
-        batch_size_per_bucket = self.batch_size // len(self.buckets)
         while True:
-            for bucket in self.buckets:
-                if len(self.aspect_ratio_bucket_indices[bucket]) >= batch_size_per_bucket:
-                    print(f'Yielding a batch for bucket {bucket}.')
-                    for _ in range(batch_size_per_bucket):
-                        yield self.aspect_ratio_bucket_indices[bucket].pop()
+            # Choose the bucket to yield from
+            bucket = self.buckets[self.current_bucket]
+            # Check if there are enough indices left in the bucket for a batch
+            if len(self.aspect_ratio_bucket_indices[bucket]) > 0:
+                print(f'Yielding a sample for bucket {bucket}.')
+                yield self.aspect_ratio_bucket_indices[bucket].pop()
+                self.current_count += 1
+                # If we've reached the batch size, move to the next bucket
+                if self.current_count >= self.batch_size:
+                    self.current_bucket = (self.current_bucket + 1) % len(self.buckets)
+                    self.current_count = 0
 
     def __len__(self):
         return sum(len(indices) for indices in self.aspect_ratio_bucket_indices.values())
