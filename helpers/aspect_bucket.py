34d56ec4af702946047461ae4de4d87656b9c6dd
bghira <bghira@users.github.com>
2023-06-17 18:33:51 -0700

    More evenly sample from aspect buckets WIP

--- helpers/aspect_bucket.py
+++ helpers/aspect_bucket.py
@@ -6,11 +6,13 @@ class BalancedBucketSampler(torch.utils.data.Sampler):
         self.buckets = list(aspect_ratio_bucket_indices.keys())
 
     def __iter__(self):
+        batch_size_per_bucket = self.batch_size // len(self.buckets)
         while True:
             for bucket in self.buckets:
-                if self.aspect_ratio_bucket_indices[bucket]:
-                    print(f'Yielding an index for bucket {bucket}.')
-                    yield self.aspect_ratio_bucket_indices[bucket].pop()
+                if len(self.aspect_ratio_bucket_indices[bucket]) >= batch_size_per_bucket:
+                    print(f'Yielding a batch for bucket {bucket}.')
+                    for _ in range(batch_size_per_bucket):
+                        yield self.aspect_ratio_bucket_indices[bucket].pop()
 
     def __len__(self):
         return sum(len(indices) for indices in self.aspect_ratio_bucket_indices.values())
