1b6e4903245e1b3c2751d502c1466cf0c0874ffd
bghira <bghira@users.github.com>
2023-07-02 11:26:52 -0700

    SNR Gamma: Fix SNR value going to NaN when using rescaled alphas

--- helpers/min_snr_gamma.py
+++ helpers/min_snr_gamma.py
@@ -1,13 +1,17 @@
 # From Diffusers repository: examples/research_projects/onnxruntime/text_to_image/train_text_to_image.py
-import torch
+import logging
 def compute_snr(timesteps, noise_scheduler):
     """
     Computes SNR as per https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/blob/521b624bd70c67cee4bdf49225915f5945a872e3/guided_diffusion/gaussian_diffusion.py#L847-L849
     """
+    minimal_value = 1e-9
     alphas_cumprod = noise_scheduler.alphas_cumprod
+    # Use .any() to check if any elements in the tensor are zero
+    if (alphas_cumprod[:-1] == 0).any():
+        logging.warning(f'Alphas cumprod has zero elements! Resetting to {minimal_value}..')
+        alphas_cumprod[alphas_cumprod[:-1] == 0] = minimal_value
     sqrt_alphas_cumprod = alphas_cumprod**0.5
     sqrt_one_minus_alphas_cumprod = (1.0 - alphas_cumprod) ** 0.5
-
     # Expand the tensors.
     # Adapted from https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/blob/521b624bd70c67cee4bdf49225915f5945a872e3/guided_diffusion/gaussian_diffusion.py#L1026
     sqrt_alphas_cumprod = sqrt_alphas_cumprod.to(device=timesteps.device)[timesteps].float()
@@ -22,14 +26,8 @@ def compute_snr(timesteps, noise_scheduler):
 
     # Compute SNR, first without epsilon
     snr = (alpha / sigma) ** 2
-
-    # Create a mask where sigma is zero
-    zero_sigma_mask = (sigma == 0)
-
-    if zero_sigma_mask.any():
-        # Compute SNR with epsilon, but only where sigma was zero
-        snr_with_epsilon = (alpha / (sigma + epsilon)) ** 2
-        # Use the mask to replace only the problematic values in snr
-        snr = torch.where(zero_sigma_mask, snr_with_epsilon, snr)
-
+    # Check if the first element in SNR tensor is zero
+    if snr[0].item() == 0:
+        logging.warning(f'First SNR tensor is zero! Resetting to {minimal_value}..')
+        snr[0] = minimal_value
     return snr
