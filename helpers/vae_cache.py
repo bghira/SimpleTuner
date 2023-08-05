e4be9c394f058891ad18fa147d6b6fd818fff942
bghira <bghira@users.github.com>
2023-08-04 21:57:55 -0700

    More latent cache logging

--- helpers/vae_cache.py
+++ helpers/vae_cache.py
@@ -4,7 +4,7 @@ from PIL import Image
 import torchvision.transforms as transforms
 
 logger = logging.getLogger('VAECache')
-logger.setLevel('INFO')
+logger.setLevel('DEBUG')
 
 class VAECache:
     def __init__(self, vae, accelerator, cache_dir="vae_cache"):
@@ -27,17 +27,20 @@ class VAECache:
     def encode_image(self, pixel_values):
         image_hash = self.create_hash(pixel_values)
         filename = os.path.join(self.cache_dir, image_hash + ".pt")
-        transform = transforms.ToTensor()
-
         if os.path.exists(filename):
             latents = self.load_from_cache(filename)
+            logger.debug(f'Loading latents of shape {latents.shape} from existing cache file: {filename}')
         else:
             with torch.no_grad():
                 latents = self.vae.encode(pixel_values.unsqueeze(0).to(self.accelerator.device, dtype=torch.bfloat16)).latent_dist.sample()
+                logger.debug(f'Using shape {latents.shape}, creating new latent cache: {filename}')
             latents = latents * self.vae.config.scaling_factor
+            logger.debug(f'Latent shape after re-scale: {latents.shape}')
             self.save_to_cache(filename, latents.squeeze())
 
-        return latents.squeeze().to(self.accelerator.device, dtype=self.vae.dtype)
+        output_latents = latents.squeeze().to(self.accelerator.device, dtype=self.vae.dtype)
+        logger.debug(f'Output latents shape: {output_latents.shape}')
+        return output_latents
 
     def process_directory(self, directory):
         # Define a transform to convert the image to tensor
@@ -45,15 +48,18 @@ class VAECache:
 
         # Get a list of all the files to process (customize as needed)
         files_to_process = []
+        logger.debug(f'Beginning processing of VAECache directory {directory}')
         for subdir, _, files in os.walk(directory):
             for file in files:
                 if file.endswith(('.png', '.jpg', '.jpeg')):
+                    logger.debug(f'Discovered image: {os.path.join(subdir, file)}')
                     files_to_process.append(os.path.join(subdir, file))
 
         # Iterate through the files, displaying a progress bar
         for filepath in tqdm(files_to_process, desc='Processing images'):
             # Open the image using PIL
             try:
+                logger.debug(f'Loading image: {filepath}')
                 image = Image.open(filepath)
                 image = image.convert('RGB')
             except Exception as e:
