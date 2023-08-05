6056d0adf604c96ddbb7d8940a33db6ded903cba
bghira <bghira@users.github.com>
2023-08-04 19:16:30 -0700

    Use a dummy text encoder in place of the real deal

--- helpers/fake_encoder.py
+++ helpers/fake_encoder.py
@@ -4,4 +4,12 @@ class FakeTextEncoder(PreTrainedModel):
     """
     def __init__(self, text_encoder, dtype):
         self.config = text_encoder.config
-        self.dtype = dtype
+        self._dtype = dtype  # Use a private attribute to store the value
+
+    @property
+    def dtype(self):  # Define a getter for the dtype property
+        return self._dtype
+
+    @dtype.setter
+    def dtype(self, value):  # Define a setter for the dtype property
+        self._dtype = value
