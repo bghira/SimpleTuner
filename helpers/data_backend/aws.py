037d1c8c8005f0e4019190589715c6d542a247c4
bghira <bghira@users.github.com>
2023-08-24 22:22:17 -0700

    S3DataBackend: should use str-casted S3 key

--- helpers/data_backend/aws.py
+++ helpers/data_backend/aws.py
@@ -32,14 +32,14 @@ class S3DataBackend(BaseDataBackend):
     def exists(self, s3_key) -> bool:
         """Determine whether a file exists in S3."""
         try:
-            self.client.head_object(Bucket=self.bucket_name, Key=s3_key)
+            self.client.head_object(Bucket=self.bucket_name, Key=str(s3_key))
             return True
         except:
             return False
 
     def read(self, s3_key):
         """Retrieve and return the content of the file from S3."""
-        response = self.client.get_object(Bucket=self.bucket_name, Key=s3_key)
+        response = self.client.get_object(Bucket=self.bucket_name, Key=str(s3_key))
         return response["Body"].read()
     
     def open_file(self, s3_key, mode):
@@ -48,11 +48,11 @@ class S3DataBackend(BaseDataBackend):
 
     def write(self, s3_key, data):
         """Upload data to the specified S3 key."""
-        self.client.put_object(Body=data, Bucket=self.bucket_name, Key=s3_key)
+        self.client.put_object(Body=data, Bucket=self.bucket_name, Key=str(s3_key))
 
     def delete(self, s3_key):
         """Delete the specified file from S3."""
-        self.client.delete_object(Bucket=self.bucket_name, Key=s3_key)
+        self.client.delete_object(Bucket=self.bucket_name, Key=str(s3_key))
 
     def list_by_prefix(self, prefix=""):
         """List all files under a specific path (prefix) in the S3 bucket."""
