c6512bdfd1d928b9822b303655faf89bf3885249
bghira <bghira@users.github.com>
2023-08-25 19:04:58 -0700

    LocalDataBackend: fix for list_files for S3 compatibility

--- helpers/data_backend/local.py
+++ helpers/data_backend/local.py
@@ -2,8 +2,9 @@ from helpers.data_backend.base import BaseDataBackend
 from pathlib import Path
 import os, logging
 
-logger = logging.getLogger('LocalDataBackend')
-logger.setLevel(os.environ.get('SIMPLETUNER_LOG_LEVEL', 'WARNING'))
+logger = logging.getLogger("LocalDataBackend")
+logger.setLevel(os.environ.get("SIMPLETUNER_LOG_LEVEL", "WARNING"))
+
 
 class LocalDataBackend(BaseDataBackend):
     def read(self, filepath):
@@ -13,7 +14,7 @@ class LocalDataBackend(BaseDataBackend):
 
     def write(self, filepath, data):
         """Write data to the specified file."""
-        # Convert data to Bytes:    
+        # Convert data to Bytes:
         if isinstance(data, str):
             data = data.encode("utf-8")
         with open(filepath, "wb") as file:
@@ -29,19 +30,22 @@ class LocalDataBackend(BaseDataBackend):
     def exists(self, filepath):
         """Check if the file exists."""
         return os.path.exists(filepath)
-    
+
     def open_file(self, filepath, mode):
         """Open the file in the specified mode."""
         return open(filepath, mode)
-    
+
     def list_files(self, str_pattern: str, instance_data_root: str):
         """
         List all files matching the pattern.
         Creates Path objects of each file found.
         """
-        logger.debug(f'LocalDataBackend.list_files: str_pattern={str_pattern}, instance_data_root={instance_data_root}')
+        logger.debug(
+            f"LocalDataBackend.list_files: str_pattern={str_pattern}, instance_data_root={instance_data_root}"
+        )
         if instance_data_root is None:
             raise ValueError("instance_data_root must be specified.")
+
         def _rglob_follow_symlinks(path: Path, pattern: str):
             for p in path.glob(pattern):
                 yield p
@@ -53,10 +57,22 @@ class LocalDataBackend(BaseDataBackend):
                     if real_path.is_dir():
                         yield from _rglob_follow_symlinks(real_path, pattern)
 
-        return list(_rglob_follow_symlinks(Path(instance_data_root), str_pattern))
+        paths = list(_rglob_follow_symlinks(Path(instance_data_root), str_pattern))
+
+        # Group files by their parent directory
+        path_dict = {}
+        for path in paths:
+            parent = str(path.parent)
+            if parent not in path_dict:
+                path_dict[parent] = []
+            path_dict[parent].append(str(path.name))
+
+        results = [(subdir, [], files) for subdir, files in path_dict.items()]
+        return results
 
     def read_image(self, filepath):
         from PIL import Image
+
         return Image.open(self.read(filepath))
 
     def create_directory(self, directory_path):
@@ -64,8 +80,10 @@ class LocalDataBackend(BaseDataBackend):
 
     def torch_load(self, filename):
         import torch
+
         return torch.load(self.read(filename))
 
     def torch_save(self, data, filename):
         import torch
-        torch.save(data, self.open_file(filename, 'wb'))
+
+        torch.save(data, self.open_file(filename, "wb"))
