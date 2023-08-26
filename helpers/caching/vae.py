6e14e2d3520307189fadb099986b3c959ced0fae
bghira <bghira@users.github.com>
2023-08-26 16:26:52 -0700

    VAECache: fix

--- helpers/caching/vae.py
+++ helpers/caching/vae.py
@@ -93,13 +93,12 @@ class VAECache:
 
         # Get a list of all existing .pt files in the directory
         existing_pt_files = set()
-        for comp1, comp2, files in self.data_backend.list_files(
+        for subdir, _, files in self.data_backend.list_files(
             instance_data_root=self.cache_dir, str_pattern="*.pt"
         ):
             for file in files:
                 logger.debug(f'Found existing .pt file: {file}')
-                logger.debug(f'Components: {comp1}, {comp2}')
-                existing_pt_files.add(os.path.join(_, file))
+                existing_pt_files.add(os.path.join(subdir, file))
 
         # Get a list of all the files to process (customize as needed)
         files_to_process = []
