51be9f1e47efc8bee165b28f8bf0a859a446a656
bghira <bghira@users.github.com>
2023-09-01 20:38:21 -0700

    SD2: add caption_strategy

--- training.sh
+++ training.sh
@@ -31,7 +31,7 @@ accelerate launch  \
   --seen_state_path="${SEEN_STATE_PATH}" \
   --state_path="${STATE_PATH}" \
   --caption_dropout_interval="${CAPTION_DROPOUT_INTERVAL}" \
-  --caption-strategy="${CAPTION_STRATEGY}"
+  --caption_strategy="${CAPTION_STRATEGY}"
 
 
   #--prepend_instance_prompt --instance_prompt="${INSTANCE_PROMPT}" \
