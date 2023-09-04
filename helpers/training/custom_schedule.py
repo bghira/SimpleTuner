914ec3bb9bed8cc70432673ce20818229e1be142
bghira <bghira@users.github.com>
2023-09-04 15:51:17 -0700

    Update the custom polynomial scheduler to properly use lr_end

--- helpers/training/custom_schedule.py
+++ helpers/training/custom_schedule.py
@@ -4,11 +4,11 @@ import torch
 
 def get_polynomial_decay_schedule_with_warmup(
     optimizer,
-    num_warmup_steps,
-    num_training_steps,
-    lr_end=1e-7,
-    power=1.0,
-    last_epoch=-1,
+    num_warmup_steps: int,
+    num_training_steps: int,
+    lr_end: float=1e-7,
+    power: float=1.0,
+    last_epoch: int=-1,
 ):
     """
     Create a schedule with a learning rate that decreases as a polynomial decay from the initial lr set in the
@@ -48,13 +48,13 @@ def get_polynomial_decay_schedule_with_warmup(
         if current_step < num_warmup_steps:
             return float(current_step) / float(max(1, num_warmup_steps))
         elif current_step > num_training_steps:
-            return lr_end / lr_init  # as LambdaLR multiplies by lr_init
+            return float(lr_end) / float(lr_init)  # as LambdaLR multiplies by lr_init
         else:
-            lr_range = lr_init - lr_end
-            decay_steps = num_training_steps - num_warmup_steps
-            pct_remaining = 1 - (current_step - num_warmup_steps) / decay_steps
-            decay = lr_range * pct_remaining**power + lr_end
-            return decay / lr_init  # as LambdaLR multiplies by lr_init
+            lr_range = float(lr_init) - float(lr_end)
+            decay_steps = int(num_training_steps) - int(num_warmup_steps)
+            pct_remaining = 1 - (current_step - int(num_warmup_steps)) / decay_steps
+            decay = lr_range * pct_remaining**power + float(lr_end)
+            return decay / float(lr_init)  # as LambdaLR multiplies by lr_init
 
     return LambdaLR(optimizer, lr_lambda, last_epoch)
 
