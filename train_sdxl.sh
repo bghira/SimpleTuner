88c5df1c4e601667881945fc6f253c51eed0be56
bghira <bghira@users.github.com>
2023-09-08 13:56:05 -0700

    Pass MAX_NUM_STEPS into train_sdxl.py via --max_train_steps

--- train_sdxl.sh
+++ train_sdxl.sh
@@ -142,7 +142,7 @@ fi
 accelerate launch ${ACCELERATE_EXTRA_ARGS} --mixed_precision="${MIXED_PRECISION}" --num_processes="${TRAINING_NUM_PROCESSES}" --num_machines="${TRAINING_NUM_MACHINES}" --dynamo_backend="${TRAINING_DYNAMO_BACKEND}" train_sdxl.py \
 	--pretrained_model_name_or_path="${MODEL_NAME}" \
 	--resume_from_checkpoint="${RESUME_CHECKPOINT}" \
-	--num_train_epochs="${NUM_EPOCHS}" \
+	--num_train_epochs=${NUM_EPOCHS} --max_train_steps=${MAX_NUM_STEPS} \
 	--learning_rate="${LEARNING_RATE}" --lr_scheduler="${LR_SCHEDULE}" --seed "${TRAINING_SEED}" \
 	--instance_data_dir="${INSTANCE_DIR}" --seen_state_path="${SEEN_STATE_PATH}" --state_path="${STATE_PATH}" --output_dir="${OUTPUT_DIR}" \
 	${DEBUG_EXTRA_ARGS}	--mixed_precision="${MIXED_PRECISION}" --vae_dtype="${MIXED_PRECISION}" ${TRAINER_EXTRA_ARGS} \
