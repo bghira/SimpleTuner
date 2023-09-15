17cfb079a2041770199d6af65ebf0d0af8e74f48
bghira <bghira@users.github.com>
2023-09-14 20:03:59 -0700

    Validations: restrict to when we have a gradient accumulation step done

--- helpers/legacy/validation.py
+++ helpers/legacy/validation.py
@@ -110,11 +110,13 @@ def log_validations(
             f" We are on step {global_step} and have {len(validation_prompts)} validation prompts."
             f" We have {progress_bar.n} steps of progress done and are resuming from {resume_global_step}."
             f" We are on step {step} of the current epoch. We have {len(validation_prompts)} validation prompts."
+            f" We have {step % args.gradient_accumulation_steps} gradient accumulation steps remaining."
         )
         if (
             validation_prompts
             and global_step % args.validation_steps == 0
             and progress_bar.n > resume_global_step
+            and (args.gradient_accumulation_steps == 1 or step % args.gradient_accumulation_steps == 0)
         ):
             logger.debug(
                 f"We might want to process validations, because we have {len(validation_prompts)} validation prompts,"
