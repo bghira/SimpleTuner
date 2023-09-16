4fa2190102a3f3431437add0f71a3bd15c622ce2
bghira <bghira@users.github.com>
2023-09-16 11:08:34 -0700

    Loading states, debug information

--- helpers/multiaspect/state.py
+++ helpers/multiaspect/state.py
@@ -1,6 +1,8 @@
 import json, os, multiprocessing, logging
-logger = logging.getLogger('BucketStateManager')
+
+logger = logging.getLogger("BucketStateManager")
 logger.setLevel(os.environ.get("SIMPLETUNER_LOG_LEVEL", "INFO"))
+
 
 class BucketStateManager:
     def __init__(self, state_path, seen_images_path):
@@ -20,25 +22,25 @@ class BucketStateManager:
 
     def deep_convert_dict(self, d):
         if isinstance(d, dict):
-            logger.debug(f'Found a dictionary to convert: {d}')
+            logger.debug(f"Found a dictionary to convert: {d}")
             return {key: self.deep_convert_dict(value) for key, value in d.items()}
         elif isinstance(d, list):
-            logger.debug(f'Found a list to convert: {d}')
+            logger.debug(f"Found a list to convert: {d}")
             return [self.deep_convert_dict(value) for value in d]
         elif isinstance(d, multiprocessing.managers.DictProxy):
-            logger.debug(f'Found a DictProxy to convert: {d}')
+            logger.debug(f"Found a DictProxy to convert: {d}")
             return self.deep_convert_dict(dict(d))
         else:
-            logger.debug(f'Returning straight-through type {type(d)}')
+            logger.debug(f"Returning straight-through type {type(d)}")
             return d
 
     def save_state(self, state: dict, state_path: str = None):
         final_state = state
         if state_path is None:
             state_path = self.state_path
-        logger.debug(f'Type of state: {type(state)}')
+        logger.debug(f"Type of state: {type(state)}")
         final_state = self.deep_convert_dict(state)
-        logger.info(f'Saving trainer state to {state_path}')
+        logger.info(f"Saving trainer state to {state_path}")
         with open(state_path, "w") as f:
             json.dump(final_state, f)
 
@@ -47,4 +49,5 @@ class BucketStateManager:
             with open(self.state_path, "r") as f:
                 return json.load(f)
         else:
+            logger.debug(f"load_state found no file.")
             return {}
