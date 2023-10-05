27ef15395bd8a7194404c95362d91f272b9f88a2
bghira <bghira@users.github.com>
2023-10-04 20:59:51 -0700

    Log erroneous files, when we try and access one

--- helpers/caching/vae.py
+++ helpers/caching/vae.py
@@ -139,8 +139,7 @@ class VAECache:
         if len(uncached_image_indices) > 0 and load_from_cache:
             # We wanted only uncached images. Something went wrong.
             raise Exception(
-                "Some images were not correctly cached during the VAE Cache operations. Ensure --skip_file_discovery=vae is not set."
-                f" Problematic images: {uncached_image_indices}"
+                f"Some images were not correctly cached during the VAE Cache operations. Ensure --skip_file_discovery=vae is not set.\nProblematic images: {uncached_image_indices}"
             )
         elif not load_from_cache:
             uncached_images = [images[i] for i in uncached_image_indices]
