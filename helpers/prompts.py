6dcaf83bc209e1c8eb8bbcb05c658dbbc71634dc
bghira <bghira@users.github.com>
2023-10-07 21:04:44 -0700

    Prompt handler text encoder detection fix (SD 2.x)

--- helpers/prompts.py
+++ helpers/prompts.py
@@ -174,7 +174,7 @@ class PromptHandler:
 
         self.accelerator = accelerator
         self.encoder_style = model_type
-        if len(text_encoders) == 2 and text_encoders[0] is not None:
+        if len(text_encoders) == 2 and text_encoders[1] is not None:
             # SDXL Refiner and Base can both use the 2nd tokenizer/encoder.
             logger.debug(f"Initialising Compel prompt manager with dual text encoders.")
             self.compel = Compel(
