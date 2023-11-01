dbac70741ddca726cbc5ccc6d2a339e6bcc8d824
bghira <bghira@users.github.com>
2023-10-31 18:35:57 -0700

    Generate default noise

--- train_sdxl.py
+++ train_sdxl.py
@@ -1048,7 +1048,7 @@ def main():
                 latents = batch["latent_batch"].to(dtype=weight_dtype)
 
                 # Sample noise that we'll add to the latents - args.noise_offset might need to be set to 0.1 by default.
-                noise = None
+                noise = torch.randn_like(latents)
                 if args.offset_noise:
                     if args.noise_offset_probability == 1.0 or random.random() < args.noise_offset_probability:
                         noise = torch.randn_like(latents) + args.noise_offset * torch.randn(
@@ -1059,8 +1059,6 @@ def main():
                 if args.input_perturbation:
                     if args.input_perturbation_probability == 1.0 or random.random() < args.input_perturbation_probability:
                         noise = noise + args.input_perturbation * torch.randn_like(noise)
-                if noise is None:
-                    noise = torch.randn_like(latents)
 
                 bsz = latents.shape[0]
                 training_logger.debug(f"Working on batch size: {bsz}")
