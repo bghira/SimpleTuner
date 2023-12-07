d2eac6debdf53d1a95ba681ec342ab5c056db4f8
Bghira Bagheera <bghira@users.github.com>
2023-12-06 19:00:58 -0600

    fix tests

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,14 +1,15 @@
 import unittest
 from helpers.multiaspect.state import BucketStateManager
 
-class TestBucketStateManager(unittest.TestCase):
 
+class TestBucketStateManager(unittest.TestCase):
     def setUp(self):
         pass  # TODO: Add setup code if needed
 
     def test_example(self):
         # TODO: Write test cases
         self.assertEqual(True, True)
+
 
 if __name__ == "__main__":
     unittest.main()
