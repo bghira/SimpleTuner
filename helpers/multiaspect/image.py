1db671926076fee10036fdaf2e1ce69e757bb5d8
bghira <bghira@users.github.com>
2023-12-22 13:04:56 -0600

    VAECache: fix overriding value of resolution, part 3

--- helpers/multiaspect/image.py
+++ helpers/multiaspect/image.py
@@ -114,6 +114,8 @@ class MultiaspectImage:
             )
             # Convert 'resolution' from eg. "1 megapixel" to "1024 pixels"
             resolution = resolution * 1e3
+            # Make resolution a multiple of 64
+            resolution = MultiaspectImage._round_to_nearest_multiple(resolution, 64)
             logging.debug(
                 f"After area resize, our image will be {target_width}x{target_height} with an overridden resolution of {resolution} pixels."
             )
