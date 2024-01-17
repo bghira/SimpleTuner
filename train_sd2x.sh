8aad40be5d744bfeebab6630f979417234bc8857
bghira <bghira@users.github.com>
2024-01-16 20:16:37 -0600

    SD2.x and SDXL reconciliation (WIP, SD 2.x is still broken)

--- train_sd2x.sh
+++ train_sd2x.sh
@@ -5,25 +5,6 @@ source sd21-env.sh.example
 # Pull config from env.sh
 source sd21-env.sh
 
-if [ -z "${PROTECT_JUPYTER_FOLDERS}" ]; then
-    # We had no value for protecting the folders, so we nuke them!
-    echo "Deleting Jupyter notebook folders in 5 seconds if you do not cancel out."
-    echo "These folders are generally useless, and will cause problems if they remain."
-    export seconds
-    seconds=4
-    for ((i=seconds;i>0;i--)); do
-        echo -n "."
-        sleep 1
-    done
-    echo "." # Newline
-    echo "YOUR TIME HAS COME."
-    if [ -n "${INSTANCE_DIR}" ]; then
-     find "${INSTANCE_DIR}" -type d -name ".ipynb_checkpoints" -exec rm -vr {} \;
-    fi
-    find "${OUTPUT_DIR}" -type d -name ".ipynb_checkpoints" -exec rm -vr {} \;
-    find "." -type d -name ".ipynb_checkpoints" -exec rm -vr {} \;
-fi
-
 if [ -z "$TRAINER_EXTRA_ARGS" ]; then
     export TRAINER_EXTRA_ARGS=""
 fi
@@ -32,7 +13,6 @@ accelerate launch  \
 --num_processes="${TRAINING_NUM_PROCESSES}" --num_machines="${TRAINING_NUM_MACHINES}" --mixed_precision="${MIXED_PRECISION}" --dynamo_backend="${TRAINING_DYNAMO_BACKEND}" \
 train_sd21.py \
 --pretrained_model_name_or_path="${MODEL_NAME}"  \
---instance_data_dir="${INSTANCE_DIR}" \
 --output_dir="${OUTPUT_DIR}" \
 --resolution="${RESOLUTION}" \
 --minimum_image_size="${MINIMUM_RESOLUTION}" \
@@ -53,8 +33,6 @@ train_sd21.py \
 --train_text_encoder --text_encoder_limit="${TEXT_ENCODER_LIMIT}" \
 --freeze_encoder --freeze_encoder_strategy="${TEXT_ENCODER_FREEZE_STRATEGY}" --freeze_encoder_before="${TEXT_ENCODER_FREEZE_BEFORE}" --freeze_encoder_after="${TEXT_ENCODER_FREEZE_AFTER}" \
 --gradient_accumulation_steps="${GRADIENT_ACCUMULATION_STEPS}" --gradient_checkpointing \
---seen_state_path="${SEEN_STATE_PATH}" \
---state_path="${STATE_PATH}" \
 --caption_dropout_probability="${CAPTION_DROPOUT_PROBABILITY}" \
 --caption_strategy="${CAPTION_STRATEGY}" \
 --data_backend_config="${DATALOADER_CONFIG}" ${TRAINER_EXTRA_ARGS}
