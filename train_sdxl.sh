56be9bb7c94dabe6ba12f66a84d59e3835c4f344
bghira <bghira@users.github.com>
2024-02-07 21:39:18 -0600

    add OPTIMIZER env var to sdxl scripts

--- train_sdxl.sh
+++ train_sdxl.sh
@@ -153,6 +153,26 @@ if [ -n "$USE_EMA" ] && [[ "$USE_EMA" == "true" ]]; then
     fi
     export EMA_ARGS="--use_ema --ema_decay=${EMA_DECAY}"
 fi
+# OPTIMIZER can be "adamw", "adamw8bit", "adafactor", "dadaptation" and we'll use case-switch to detect and set --use_8bit_adam, --use_adafactor_optimizer, --use_dadaptation_optimizer or nothing for plain adam.
+export OPTIMIZER_ARG=""
+case $OPTIMIZER in
+    "adamw")
+        export OPTIMIZER_ARG=""
+        ;;
+    "adamw8bit")
+        export OPTIMIZER_ARG="--use_8bit_adam"
+        ;;
+    "adafactor")
+        export OPTIMIZER_ARG="--use_adafactor_optimizer"
+        ;;
+    "dadaptation")
+        export OPTIMIZER_ARG="--use_dadaptation_optimizer"
+        ;;
+    *)
+        echo "Unknown optimizer requested: $OPTIMIZER"
+        exit 1
+        ;;
+esac
 
 export DELETE_ARGS=""
 if ! [ -z "$DELETE_SMALL_IMAGES" ] && [ $DELETE_SMALL_IMAGES -eq 1 ]; then
@@ -215,7 +235,7 @@ accelerate launch ${ACCELERATE_EXTRA_ARGS} --mixed_precision="${MIXED_PRECISION}
 --pretrained_model_name_or_path="${MODEL_NAME}" ${XFORMERS_ARG} ${GRADIENT_ARG} --set_grads_to_none --gradient_accumulation_steps=${GRADIENT_ACCUMULATION_STEPS} \
 --resume_from_checkpoint="${RESUME_CHECKPOINT}" ${DELETE_ARGS} ${SNR_GAMMA_ARG} --data_backend_config="${DATALOADER_CONFIG}" \
 --num_train_epochs=${NUM_EPOCHS} --max_train_steps=${MAX_NUM_STEPS} --metadata_update_interval=${METADATA_UPDATE_INTERVAL} \
---learning_rate="${LEARNING_RATE}" --lr_scheduler="${LR_SCHEDULE}" --seed "${TRAINING_SEED}" --lr_warmup_steps="${LR_WARMUP_STEPS}" \
+${OPTIMIZER_ARG} --learning_rate="${LEARNING_RATE}" --lr_scheduler="${LR_SCHEDULE}" --seed "${TRAINING_SEED}" --lr_warmup_steps="${LR_WARMUP_STEPS}" \
 --output_dir="${OUTPUT_DIR}" \
 --inference_scheduler_timestep_spacing="${INFERENCE_SCHEDULER_TIMESTEP_SPACING}" --training_scheduler_timestep_spacing="${TRAINING_SCHEDULER_TIMESTEP_SPACING}" \
 ${DEBUG_EXTRA_ARGS}	${TF32_ARG} --mixed_precision="${MIXED_PRECISION}" --vae_dtype="${MIXED_PRECISION}" ${TRAINER_EXTRA_ARGS} \
