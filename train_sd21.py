9e13cf3962f3a726c579546b00b7186bd009d087
bghira <bghira@users.github.com>
2024-02-10 23:16:31 -0600

    adamw: fix passing LR into AdamW (non-8bit)

--- train_sd21.py
+++ train_sd21.py
@@ -499,6 +499,8 @@ def main():
     else:
         logger.info("Using AdamW optimizer.")
         optimizer_class = torch.optim.AdamW
+        extra_optimizer_args["betas"] = (args.adam_beta1, args.adam_beta2)
+        extra_optimizer_args["lr"] = args.learning_rate
 
     # Optimizer creation
     if args.model_type == "full":
