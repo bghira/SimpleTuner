07292b3deece0f7f0ffd45aa6c15e1413088813f
bghira <bghira@users.github.com>
2024-02-20 23:44:29 -0600

    cogvlm: distributed captioning scripts

---
+++ toolkit/captioning/caption_backend_server.php
@@ -0,0 +1,93 @@
+<?php
+
+// Load MySQL credentials
+$credentials = json_decode(file_get_contents("/var/www/.mysql.json"), true);
+$users = json_decode(file_get_contents("/var/www/.users.json"), true);
+
+// Create PDO connection
+$dsn = "mysql:host={$credentials['host']};dbname={$credentials['database']};charset=utf8mb4";
+try {
+    $pdo = new PDO($dsn, $credentials['user'], $credentials['password'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    die("Could not connect to the database (". $dsn ."): " . $e->getMessage());
+}
+
+// Create the `dataset` table if it does not exist
+$pdo->exec("CREATE TABLE IF NOT EXISTS dataset (
+    data_id INT AUTO_INCREMENT PRIMARY KEY,
+    URL VARCHAR(255) NOT NULL,
+    pending BOOLEAN NOT NULL DEFAULT 0,
+    result TEXT,
+    submitted_at DATETIME,
+    attempts INT DEFAULT 0,
+    error TEXT
+)");
+
+// Action handling
+$action = $_GET['action'] ?? '';
+$client_id = $_GET['client_id'];
+$secret = $_GET['secret'];
+
+if (!in_array($client_id, array_keys($users)) || $secret !== $users[$client_id]) {
+        http_response_code(403);
+        echo "Unauthorized.";
+        exit;
+}
+
+$filter_strings = [
+    '</s>',
+    'The image showcases '
+];
+
+try {
+    switch ($action) {
+        case 'list_jobs':
+            $count = $_GET['count'] ?? 1;
+            $stmt = $pdo->prepare("SELECT * FROM dataset WHERE pending = 0 OR (submitted_at IS NOT NULL AND submitted_at < NOW() - INTERVAL 1 HOUR) AND result IS NULL LIMIT ?");
+            $stmt->bindValue(1, $count, PDO::PARAM_INT);
+            $stmt->execute();
+            $jobs = $stmt->fetchAll();
+            
+            // Update pending and submitted_at for retrieved jobs
+           foreach ($jobs as $job) {
+               $updateStmt = $pdo->prepare("UPDATE dataset SET pending = 1, submitted_at = NOW(), attempts = attempts + 1 WHERE data_id = ?");
+               $updateStmt->execute([$job['data_id']]);
+           }
+            
+            echo json_encode($jobs);
+            break;
+        
+        case 'submit_job':
+            $dataId = $_REQUEST['job_id'] ?? '';
+            $result = $_REQUEST['result'] ?? '';
+            $caption_source = $_REQUEST['caption_source'] ?? '';
+            $status = $_REQUEST['status'] ?? 'success';
+            $error = $_REQUEST['error'] ?? '';
+
+            if ($status == 'error' && !$error) {
+                echo "Error message required for status 'error'";
+                exit;
+            }
+
+            if (!$result || !$dataId) {
+                echo "Job ID and result are required";
+                exit;
+            }
+
+            // Clean any of the strings found in $filter_strings
+            $result = str_replace($filter_strings, '', $result);
+
+            $updateStmt = $pdo->prepare("UPDATE dataset SET client_id = ?, result = ?, pending = 0, error = ?, caption_source = ? WHERE data_id = ?");
+            $updateStmt->execute([$client_id, $result, $error, $caption_source, $dataId]);
+            echo "Job submitted successfully";
+            break;
+
+        default:
+            echo "Invalid action";
+    }
+} catch (\Throwable $ex) {
+    echo "An error occurred: " . $ex->getMessage();
+}
---
+++ toolkit/captioning/caption_with_cogvlm_remote.py
@@ -0,0 +1,239 @@
+import os, torch, logging, accelerate, re, random, argparse, io, base64
+from PIL import Image
+import requests, time
+from botocore.config import Config
+
+logger = logging.getLogger("Captioner")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Process images and generate captions."
+    )
+    parser.add_argument(
+        "--precision",
+        type=str,
+        choices=["bf16", "fp16", "fp4", "fp8"],
+        default="fp8",
+        help=(
+            "When loading CogVLM, you can load it in fp16, bf16, fp8 or fp4 precision to reduce memory. Default: fp4"
+        ),
+    )
+    parser.add_argument(
+        "--query_str",
+        type=str,
+        default="Caption this image accurately, without speculation. Just describe what you see.",
+        help="The query string to use for captioning. This instructs the model how to behave.",
+    )
+    parser.add_argument(
+        "--backend_url",
+        type=str,
+        required=True,
+        help=(
+            "The URL of the backend to use for processing. This should be the URL of the backend that will be used to process the images."
+        ),
+    )
+    parser.add_argument(
+        "--client_id",
+        type=str,
+        required=True,
+        help=("The client ID to use to login to the backend."),
+    )
+    parser.add_argument(
+        "--secret",
+        type=str,
+        required=True,
+        help=("The secret to use to login to the backend."),
+    )
+    parser.add_argument(
+        "--eval_backend",
+        type=str,
+        default="transformers",
+        choices=["oobabooga", "transformers"],
+        help=(
+            "If transformers is provided, it will load the model using the transformers library. If oobabooga is provided, it will use the API for a running installation of Oobabooga's text-generation-webui."
+        ),
+    )
+    return parser.parse_args()
+
+
+def eval_image(
+    image: Image,
+    model,
+    tokenizer,
+    torch_dtype,
+    query: str,
+):
+    inputs = model.build_conversation_input_ids(
+        tokenizer, query=query, history=[], images=[image]
+    )  # chat mode
+    inputs = {
+        "input_ids": inputs["input_ids"].unsqueeze(0).to("cuda"),
+        "token_type_ids": inputs["token_type_ids"].unsqueeze(0).to("cuda"),
+        "attention_mask": inputs["attention_mask"].unsqueeze(0).to("cuda"),
+        "images": [[inputs["images"][0].to("cuda").to(torch_dtype)]],
+    }
+    gen_kwargs = {"max_new_tokens": 77, "do_sample": False}
+
+    with torch.no_grad():
+        outputs = model.generate(**inputs, **gen_kwargs)
+        outputs = outputs[:, inputs["input_ids"].shape[1] :]
+        return tokenizer.decode(outputs[0])
+
+
+def eval_image_with_ooba(image: Image, query: str) -> str:
+    CONTEXT = "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.\n"
+
+    img_str = base64.b64encode(image).decode("utf-8")
+    prompt = (
+        CONTEXT
+        + f'### Human: {query} \n<img src="data:image/jpeg;base64,{img_str}">### Assistant: '
+    )
+
+    data = {
+        "mode": "instruct",  # chat, instruct
+        "character": "Example",
+        #           "instruction_template": "LLaVA-v1",
+        "messages": [{"role": "system", "content": prompt}],
+    }
+
+    response = requests.post("http://127.0.0.1:5000/v1/chat/completions", json=data)
+
+    if response.status_code != 200:
+        print(
+            f"Request failed with status {response.status_code}. Response: {response.text}"
+        )
+        import sys
+
+        sys.exit(1)
+    else:
+        return response.json()["choices"][0]["message"]["content"]
+
+
+def main():
+    args = parse_args()
+    logging.basicConfig(level=logging.INFO)
+
+    if args.eval_backend == "transformers":
+        send_to_cuda = load_in_4bit = load_in_8bit = False
+        torch_dtype = torch.bfloat16
+        if args.precision == "bf16" or args.precision == "fp16":
+            send_to_cuda = True
+            if args.precision == "bf16":
+                torch_dtype = torch.bfloat16
+            else:
+                torch_dtype = torch.float16
+        elif args.precision == "fp4":
+            load_in_4bit = True
+        elif args.precision == "fp8":
+            load_in_8bit = True
+
+        logger.info("Loading CogVLM model. This should only occur once.")
+        from transformers import AutoModelForCausalLM, LlamaTokenizer
+
+        tokenizer = LlamaTokenizer.from_pretrained("lmsys/vicuna-7b-v1.5")
+        logger.info(f"Loading CogVLM in {args.precision} precision.")
+        model = AutoModelForCausalLM.from_pretrained(
+            "THUDM/cogvlm-chat-hf",
+            torch_dtype=torch_dtype,
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
+            load_in_4bit=load_in_4bit,
+            load_in_8bit=load_in_8bit,
+        ).eval()
+        if send_to_cuda:
+            logger.info(f"Sending model to CUDA.")
+            model.to("cuda")
+        logger.info("Completed loading model.")
+
+    # Query backend for tasks, and loop.
+    while True:
+        # Query backend for tasks
+        response = requests.get(
+            f"{args.backend_url}/?action=list_jobs",
+            timeout=10,
+            params={"client_id": args.client_id, "secret": args.secret},
+        )
+        # 403? Exit.
+        if response.status_code == 403:
+            logger.error("Access denied. Exiting.")
+            break
+        # 500? Wait.
+        if response.status_code == 500:
+            logger.error("Server error. Waiting.")
+            time.sleep(30)
+            continue
+        # Decode the JSON response?
+        try:
+            response_json = response.json()
+        except:
+            logger.error("Could not decode JSON response. Exiting.")
+            break
+        # Example:
+        # [
+        #     {
+        #         "data_id": 474,
+        #         "URL": "https://images.pexels.com/photos/474/black-and-white-car-vehicle-vintage.jpg?cs=srgb&dl=pexels-gratisography-474.jpg&fm=jpg",
+        #         "pending": 0,
+        #         "result": None,
+        #         "submitted_at": None,
+        #         "attempts": 0,
+        #         "error": None,
+        #         "client_id": None,
+        #         "updated_at": "2024-02-21 02:32:32",
+        #     }
+        # ]
+
+        # Now, we evaluate the caption for each image.
+        for task in response_json:
+            # Skip if the task is pending or has a result
+            if task["pending"] == 1 or task["result"]:
+                continue
+            # Load the image
+            try:
+                logger.info(f"Loading image from {task['URL']}.")
+                response = requests.get(task["URL"], timeout=10)
+                image = Image.open(io.BytesIO(response.content))
+            except Exception as e:
+                logger.error(f"Could not load image from {task['URL']}.")
+                # Upload the error using endpoint?action=submit_job&job_id=data_id&error=message&status=error
+                requests.post(
+                    f"{args.backend_url}/?action=submit_job",
+                    params={
+                        "client_id": args.client_id,
+                        "secret": args.secret,
+                        "error": e,
+                        "job_id": {task["data_id"]},
+                    },
+                )
+                continue
+            # Generate the caption
+            caption_source = "cogvlm"
+            if args.eval_backend == "transformers":
+                caption = eval_image(
+                    image, model, tokenizer, torch_dtype, args.query_str
+                )
+            elif args.eval_backend == "oobabooga":
+                # only really llava is supported by oobabooga, so we will assume here.
+                caption_source = "llava"
+                image_to_bytes = io.BytesIO()
+                image.save(image_to_bytes, format="JPEG")
+                image_to_bytes = image_to_bytes.getvalue()
+                caption = eval_image_with_ooba(image_to_bytes, args.query_str)
+            # Upload the caption using endpoint?action=submit_job&job_id=data_id&result=caption&status=success
+            submission_response = requests.post(
+                f"{args.backend_url}/?action=submit_job",
+                params={
+                    "result": caption,
+                    "job_id": task["data_id"],
+                    "client_id": args.client_id,
+                    "secret": args.secret,
+                    "caption_source": caption_source,
+                    "status": "success",
+                },
+            )
+            logger.info(f"Result: {caption}")
+
+
+if __name__ == "__main__":
+    main()
