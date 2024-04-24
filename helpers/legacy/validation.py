079b59b65d58ab870a89d8d5d54b6ab2ec2db7cf
bghira <bghira@users.github.com>
2024-04-23 19:58:00 -0600

    validations: fix df stage 2

--- helpers/legacy/validation.py
+++ helpers/legacy/validation.py
@@ -497,7 +497,7 @@ def log_validations(
                     # Resize the input sample so that we can validate the model's upscaling performance.
                     width, height, _ = (
                         MultiaspectImage.calculate_new_size_by_pixel_edge(
-                            validation_sample.size[0], validation_sample.size[1], 64
+                            validation_sample.size[0], validation_sample.size[1]
                         )
                     )
                     extra_validation_kwargs["image"] = validation_sample.resize(
