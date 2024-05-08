c062d1bd7a048539d6a99367f285a26ed0dae1b9
bghira <bghira@users.github.com>
2024-05-08 17:02:30 -0600

    add information about pushing to hub

--- helpers/legacy/metadata.py
+++ helpers/legacy/metadata.py
@@ -96,7 +96,7 @@ The text encoder {'**was**' if train_text_encoder else '**was not**'} trained.
   - Gradient accumulation steps: {StateTracker.get_args().gradient_accumulation_steps}
 - Prediction type: {StateTracker.get_args().prediction_type}
 - Rescaled betas zero SNR: {StateTracker.get_args().rescale_betas_zero_snr}
-- Optimizer: {'AdamW, stochastic bf16' if StateTracker.get_args().adamw_bfloat16 else 'AdamW8Bit' if StateTracker.get_args().use_8bit_adam else 'Adafactor' if StateTracker.get_args().use_adafactor_optimizer else 'Prodigy' if StateTracker.get_args().use_prodigy_optimizer else 'AdamW'}
+- Optimizer: {'AdamW, stochastic bf16' if StateTracker.get_args().adam_bfloat16 else 'AdamW8Bit' if StateTracker.get_args().use_8bit_adam else 'Adafactor' if StateTracker.get_args().use_adafactor_optimizer else 'Prodigy' if StateTracker.get_args().use_prodigy_optimizer else 'AdamW'}
 
 ## Datasets
 
