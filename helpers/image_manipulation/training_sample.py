ff16e3c07b8c9baed39007e91ed4f66758c13416
bghira <bghira@users.github.com>
2024-05-08 19:36:18 -0600

    update training sample crop logic for simplification

--- helpers/image_manipulation/training_sample.py
+++ helpers/image_manipulation/training_sample.py
@@ -256,14 +256,17 @@ class TrainingSample:
         self._downsample_before_crop()
         self.calculate_target_size(downsample_before_crop=False)
         logger.debug(
-            f"Pre-crop size: {self.image.size if hasattr(self.image, 'size') else 'Unknown'}."
+            f"Pre-crop size: {self.image.size if hasattr(self.image, 'size') else self.target_size}."
         )
         self.cropper.set_image(self.image)
+        self.cropper.set_intermediary_size(
+            self.intermediary_size[0], self.intermediary_size[1]
+        )
         self.image, self.crop_coordinates = self.cropper.crop(
             self.target_size[0], self.target_size[1]
         )
         logger.debug(
-            f"Post-crop size: {self.image.size if hasattr(self.image, 'size') else 'Unknown'}."
+            f"Post-crop size: {self.image.size if hasattr(self.image, 'size') else self.target_size}."
         )
         return self
 
@@ -276,51 +279,40 @@ class TrainingSample:
         """
         current_size = self.image.size if self.image is not None else self.original_size
         if size is None:
-            target_size, intermediary_size, target_aspect_ratio = (
+            self.target_size, self.intermediary_size, self.target_aspect_ratio = (
                 self.calculate_target_size()
             )
-            size = target_size
-            if target_size != intermediary_size:
+            size = self.target_size
+            if self.target_size != self.intermediary_size:
                 # Now we can resize the image to the intermediary size.
                 logger.debug(
-                    f"Before resizing to {intermediary_size}, our image is {current_size} resolution."
+                    f"Before resizing to {self.intermediary_size}, our image is {current_size} resolution."
                 )
                 if self.image is not None:
                     self.image = self.image.resize(
-                        intermediary_size, Image.Resampling.LANCZOS
+                        self.intermediary_size, Image.Resampling.LANCZOS
                     )
                     logger.debug(f"After resize, we are at {self.image.size}")
-                # Crop the image to its target size, so that we do not squish or stretch the image.
-                original_crop_coordinates = self.crop_coordinates
-                if self.image is not None:
-                    logger.debug(
-                        f"It's our lucky day, the cropper has an actual image to adjust: {self.image_metadata}"
-                    )
-                    self.cropper.set_image(self.image, self.image_metadata)
-                else:
-                    logger.debug(
-                        "We are adjusting based on a dream of the image metadata, we do not have a real image."
-                    )
-                    self.image_metadata["current_size"] = intermediary_size
-                    self.cropper.set_image_metadata(self.image_metadata)
-                self.image, new_crop_coordinates = self.cropper.crop(
-                    target_size[0], target_size[1]
-                )
-                # Adjust self.crop_coordinates to adjust the crop to the new size.
-                self.crop_coordinates = (
-                    self.crop_coordinates[0] + new_crop_coordinates[0],
-                    self.crop_coordinates[1] + new_crop_coordinates[1],
+                self.cropper.set_image(self.image)
+                self.cropper.set_intermediary_size(
+                    self.intermediary_size[0], self.intermediary_size[1]
                 )
                 logger.debug(
-                    f"After crop-adjusting pixel alignment, our image is now {self.image_metadata['current_size'] if 'current_size' in self.image_metadata else self.image.size} resolution and its crop coordinates are now {self.crop_coordinates} adjusted from {original_crop_coordinates}"
+                    f"Setting intermediary size to {self.intermediary_size} for image {self.image}"
+                )
+                self.image, self.crop_coordinates = self.cropper.crop(
+                    self.target_size[0], self.target_size[1]
+                )
+                logger.debug(
+                    f"After crop-adjusting pixel alignment, our image is now {self.image_metadata['current_size'] if 'current_size' in self.image_metadata else self.image.size} resolution and its crop coordinates are now {self.crop_coordinates}"
                 )
 
             logger.debug(
-                f"Resizing image from {self.image.size if self.image is not None and type(self.image) is not dict else intermediary_size} to final target size: {size}"
+                f"Resizing image from {self.image.size if self.image is not None and type(self.image) is not dict else self.intermediary_size} to final target size: {size}"
             )
         else:
             logger.debug(
-                f"Resizing image from {self.image.size if self.image is not None and type(self.image) is not dict else intermediary_size} to custom-provided size: {size}"
+                f"Resizing image from {self.image.size if self.image is not None and type(self.image) is not dict else self.intermediary_size} to custom-provided size: {size}"
             )
         if self.image and hasattr(self.image, "resize"):
             self.image = self.image.resize(size, Image.Resampling.LANCZOS)
