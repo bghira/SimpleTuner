aceaf3e0b69de83f5a5864a5c40676982f4497cc
bghira <bghira@users.github.com>
2024-07-20 10:27:11 -0600

    pixart: allow input of image for img2img in pipeline

--- helpers/pixart/pipeline.py
+++ helpers/pixart/pipeline.py
@@ -21,7 +21,7 @@ from typing import Callable, List, Optional, Tuple, Union
 import torch
 from transformers import T5EncoderModel, T5Tokenizer
 
-from diffusers.image_processor import PixArtImageProcessor
+from diffusers.image_processor import PixArtImageProcessor, PipelineImageInput
 from diffusers.models import AutoencoderKL, PixArtTransformer2DModel
 from diffusers.schedulers import KarrasDiffusionSchedulers
 from diffusers.utils import (
@@ -732,6 +732,7 @@ class PixArtSigmaPipeline(DiffusionPipeline):
         latents=None,
         timestep=None,
         add_noise=False,
+        image=None,
     ):
         shape = (
             batch_size,
@@ -760,9 +761,72 @@ class PixArtSigmaPipeline(DiffusionPipeline):
                 latents = self.scheduler.add_noise(latents, noise, timestep)
 
         # scale the initial noise by the standard deviation required by the scheduler
-        latents = latents * self.scheduler.init_noise_sigma
+        init_latents = latents * self.scheduler.init_noise_sigma
 
-        return latents
+        if image is not None:
+            if image.shape[1] == 4:
+                init_latents = image
+
+            else:
+                # make sure the VAE is in float32 mode, as it overflows in float16
+                if self.vae.config.force_upcast:
+                    image = image.float()
+                    self.vae.to(dtype=torch.float32)
+
+                if isinstance(generator, list) and len(generator) != batch_size:
+                    raise ValueError(
+                        f"You have passed a list of generators of length {len(generator)}, but requested an effective batch"
+                        f" size of {batch_size}. Make sure the batch size matches the length of the generators."
+                    )
+
+                elif isinstance(generator, list):
+                    init_latents = [
+                        retrieve_latents(
+                            self.vae.encode(image[i : i + 1]), generator=generator[i]
+                        )
+                        for i in range(batch_size)
+                    ]
+                    init_latents = torch.cat(init_latents, dim=0)
+                else:
+                    init_latents = retrieve_latents(
+                        self.vae.encode(image), generator=generator
+                    )
+
+                if self.vae.config.force_upcast:
+                    self.vae.to(dtype)
+
+                init_latents = init_latents.to(dtype)
+                if latents_mean is not None and latents_std is not None:
+                    latents_mean = latents_mean.to(device=device, dtype=dtype)
+                    latents_std = latents_std.to(device=device, dtype=dtype)
+                    init_latents = (
+                        (init_latents - latents_mean)
+                        * self.vae.config.scaling_factor
+                        / latents_std
+                    )
+                else:
+                    init_latents = self.vae.config.scaling_factor * init_latents
+
+            if (
+                batch_size > init_latents.shape[0]
+                and batch_size % init_latents.shape[0] == 0
+            ):
+                # expand init_latents for batch_size
+                additional_image_per_prompt = batch_size // init_latents.shape[0]
+                init_latents = torch.cat(
+                    [init_latents] * additional_image_per_ompt, dim=0
+                )
+            elif (
+                batch_size > init_latents.shape[0]
+                and batch_size % init_latents.shape[0] != 0
+            ):
+                raise ValueError(
+                    f"Cannot duplicate `image` of batch size {init_latents.shape[0]} to {batch_size} text prompts."
+                )
+            else:
+                init_latents = torch.cat([init_latents], dim=0)
+
+        return init_latents
 
     @property
     def denoising_start(self):
@@ -794,6 +858,7 @@ class PixArtSigmaPipeline(DiffusionPipeline):
         width: Optional[int] = None,
         eta: float = 0.0,
         generator: Optional[Union[torch.Generator, List[torch.Generator]]] = None,
+        image: Optional[PipelineImageInput] = None,
         latents: Optional[torch.Tensor] = None,
         prompt_embeds: Optional[torch.Tensor] = None,
         prompt_attention_mask: Optional[torch.Tensor] = None,
@@ -995,6 +1060,10 @@ class PixArtSigmaPipeline(DiffusionPipeline):
         )
 
         # 5. Prepare latents.
+        if image is not None:
+            image = self.image_processor.preprocess(image)
+            image = image.to(device=device, dtype=dtype)
+
         latent_channels = self.transformer.config.in_channels
         latent_timestep = None
         if denoising_end is not None or denoising_start is not None:
@@ -1026,6 +1095,7 @@ class PixArtSigmaPipeline(DiffusionPipeline):
                 latents,
                 timestep=latent_timestep,
                 add_noise=add_noise,
+                image=image,
             )
 
         # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
@@ -1061,6 +1131,9 @@ class PixArtSigmaPipeline(DiffusionPipeline):
                 num_inference_steps = (
                     (timesteps < discrete_timestep_cutoff).sum().item()
                 )
+                print(
+                    f"Beginning inference for stage2 with {num_inference_steps} steps."
+                )
 
             else:
                 raise ValueError(
@@ -1079,6 +1152,9 @@ class PixArtSigmaPipeline(DiffusionPipeline):
                 )
                 num_inference_steps = len(
                     list(filter(lambda ts: ts >= discrete_timestep_cutoff, timesteps))
+                )
+                print(
+                    f"Beginning inference for stage1 with {num_inference_steps} steps."
                 )
                 timesteps = timesteps[:num_inference_steps]
             else:
