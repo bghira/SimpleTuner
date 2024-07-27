277ae228eadaa2ee5931a983aa4329b09004aa5e
bghira <bghira@users.github.com>
2024-07-26 22:58:27 -0600

    debugging

--- helpers/caching/vae.py
+++ helpers/caching/vae.py
@@ -139,11 +139,18 @@ class VAECache:
         base_filename = str(base_filename) + ".pt"
         # Find the subfolders the sample was in, and replace the instance_data_root with the cache_dir
         subfolders = os.path.dirname(filepath).replace(self.instance_data_root, "")
+        print(f"Subfolders: {subfolders}")
         if len(subfolders) > 0 and subfolders[0] == "/":
             subfolders = subfolders[1:]
             full_filename = os.path.join(self.cache_dir, subfolders, base_filename)
+            print(
+                f"full_filename: {full_filename} = os.path.join({self.cache_dir}, {subfolders}, {base_filename})"
+            )
         else:
             full_filename = os.path.join(self.cache_dir, base_filename)
+            print(
+                f"full_filename: {full_filename} = os.path.join({self.cache_dir}, {base_filename})"
+            )
         return full_filename, base_filename
 
     def _image_filename_from_vaecache_filename(self, filepath: str) -> tuple[str, str]:
