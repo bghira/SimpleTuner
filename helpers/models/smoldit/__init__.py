7f9376ba8d751bbf11308d31daef41e7747b1abb
bghira <bghira@users.github.com>
2024-07-30 08:07:55 -0600

    smoldit: config update

--- helpers/models/smoldit/__init__.py
+++ helpers/models/smoldit/__init__.py
@@ -2,12 +2,12 @@ from helpers.models.smoldit.transformer import SmolDiT2DModel
 from helpers.models.smoldit.pipeline import SmolDiTPipeline
 
 SmolDiTDefaultConfig = {
-    "sample_size": 16,
-    "num_layers": 2,
+    "sample_size": 64,
+    "num_layers": 16,
     "patch_size": 2,
-    "attention_head_dim": 8,
-    "num_attention_heads": 4,
-    "num_kv_heads": 2,
+    "attention_head_dim": 64,
+    "num_attention_heads": 32,
+    "num_kv_heads": 8,
     "in_channels": 4,
     "cross_attention_dim": 768,
     "out_channels": 4,
