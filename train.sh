d293f38d3321f60e20405b3a02210d31e1ae6363
bghira <bghira@users.github.com>
2024-08-07 17:25:56 -0600

    add VENV_PATH variable to train.sh which discovers the nvjitlink lib path, and override LD_LIBRARY_PATH to ensure it is loaded first

--- train.sh
+++ train.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
+
 # Pull the default config.
 [ -f "config/config.env.example" ] && source config/config.env.example
 # Pull config from config.env
 [ -f "config/config.env" ] && source config/config.env
+
+# If the user has not provided VENV_PATH, we will assume $(pwd)/.venv
+if [ -z "${VENV_PATH}" ]; then
+    export VENV_PATH="$(pwd)/.venv"
+fi
+if [ -z "${DISABLE_LD_OVERRIDE}" ]; then
+    export NVJITLINK_PATH="$(find "${VENV_PATH}" -name nvjitlink -type d)"
+    # if it's not empty, we will add it to LD_LIBRARY_PATH at the front:
+    if [ -n "${NVJITLINK_PATH}" ]; then
+        export LD_LIBRARY_PATH="${NVJITLINK_PATH}:${LD_LIBRARY_PATH}"
+    fi
+    echo $NVJITLINK_PATH
+fi
 
 export PLATFORM
 PLATFORM=$(uname -s)
