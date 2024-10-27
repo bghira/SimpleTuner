f7567f7487a17b2b3e6c99260f26c69a32754083
Mikael Hirki <mikael.hirki@gmail.com>
2024-10-27 15:53:09 +0200

    Give --flux_schedule_auto_shift parameter priority over --flux_schedule_shift. This prevents old configs from breaking after commit 4a7d42320e4457be7e52540cac1df191a1b88596 added a new default value for --flux_schedule_shift.

--- helpers/models/flux/__init__.py
+++ helpers/models/flux/__init__.py
@@ -14,7 +14,7 @@ def apply_flux_schedule_shift(args, noise_scheduler, sigmas, noise):
     if args.flux_schedule_shift is not None and args.flux_schedule_shift > 0:
         # Static shift value for every resolution
         shift = args.flux_schedule_shift
-    elif args.flux_schedule_auto_shift:
+    if args.flux_schedule_auto_shift:
         # Resolution-dependent shift value calculation used by official Flux inference implementation
         image_seq_len = (noise.shape[-1] * noise.shape[-2]) // 4
         mu = calculate_shift_flux(
