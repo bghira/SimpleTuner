d02ea241da4fed2dd6692e6ad8c7139badf207a5
bghira <bghira@users.github.com>
2025-03-31 19:16:16 -0600

    add proper PEFT LoRA support to Sana model class

--- helpers/models/sana/transformer.py
+++ helpers/models/sana/transformer.py
@@ -18,6 +18,7 @@ import torch
 from torch import nn
 
 from diffusers.configuration_utils import ConfigMixin, register_to_config
+from diffusers.loaders import PeftAdapterMixin
 from diffusers.utils import (
     USE_PEFT_BACKEND,
     is_torch_version,
@@ -201,7 +202,7 @@ class SanaTransformerBlock(nn.Module):
         return hidden_states
 
 
-class SanaTransformer2DModel(ModelMixin, ConfigMixin):
+class SanaTransformer2DModel(ModelMixin, ConfigMixin, PeftAdapterMixin):
     r"""
     A 2D Transformer model introduced in [Sana](https://huggingface.co/papers/2410.10629) family of models.
 
@@ -242,6 +243,7 @@ class SanaTransformer2DModel(ModelMixin, ConfigMixin):
 
     _supports_gradient_checkpointing = True
     _no_split_modules = ["SanaTransformerBlock", "PatchEmbed"]
+    _skip_layerwise_casting_patterns = ["patch_embed", "norm"]
 
     @register_to_config
     def __init__(
