1cc4075bb44d62b81dcfc9b42452ea475592d890
bghira <bghira@users.github.com>
2025-04-15 06:52:28 +0100

    formatting

--- helpers/models/hidream/model.py
+++ helpers/models/hidream/model.py
@@ -309,7 +309,10 @@ class HiDream(ImageModelFoundation):
                 f"{self.NAME} does not support fp8-quanto. Please use fp8-torchao or int8 precision level instead."
             )
         t5_max_length = 128
-        if self.config.tokenizer_max_length is None or self.config.tokenizer_max_length == 0:
+        if (
+            self.config.tokenizer_max_length is None
+            or self.config.tokenizer_max_length == 0
+        ):
             logger.warning(
                 f"Setting T5 XXL tokeniser max length to {t5_max_length} for {self.NAME}."
             )
