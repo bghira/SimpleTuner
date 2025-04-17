7b5f26f620af7fb9bea90518d852d2610ad290e6
bghira <bghira@users.github.com>
2025-04-17 16:26:52 +0100

    by default, prefer to resume from checkpoint

--- helpers/configuration/cmd_args.py
+++ helpers/configuration/cmd_args.py
@@ -1110,7 +1110,7 @@ def get_argument_parser():
     parser.add_argument(
         "--resume_from_checkpoint",
         type=str,
-        default=None,
+        default="latest",
         help=(
             "Whether training should be resumed from a previous checkpoint. Use a path saved by"
             ' `--checkpointing_steps`, or `"latest"` to automatically select the last available checkpoint.'
