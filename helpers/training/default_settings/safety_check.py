5e05e96549e5ead7772298b0f63cd4aefc94e6d4
bghira <bghira@users.github.com>
2025-06-15 19:55:04 +0100

    enable LoRA and ControlNet training for pixart sigma

--- helpers/training/default_settings/safety_check.py
+++ helpers/training/default_settings/safety_check.py
@@ -50,13 +50,10 @@ def safety_check(args, accelerator):
     ):
         validate_deepspeed_compat_from_args(accelerator, args)
     if args.controlnet:
-        if args.model_family not in ["sd1x", "sd2x", "sdxl", "flux"]:
+        if args.model_family not in ["sd1x", "sd2x", "sdxl", "flux", "pixart_sigma"]:
             raise ValueError(
                 f"ControlNet is not yet supported with {args.model_family} models. Please disable --controlnet, or switch model types."
             )
-    if "lora" in args.model_type and "standard" == args.lora_type.lower():
-        if args.model_family == "pixart_sigma":
-            raise Exception(f"{args.model_type} does not support LoRA model training.")
 
     if "lora" in args.model_type and args.train_text_encoder:
         if args.lora_type.lower() == "lycoris":
