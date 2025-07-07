cc777ba2d3bdb5fbbdfb3e914cd16cff1760eda2
bghira <bghira@users.github.com>
2025-07-07 07:41:38 -0600

    fix decompression failure not getting caught

--- helpers/data_backend/base.py
+++ helpers/data_backend/base.py
@@ -131,7 +131,11 @@ class BaseDataBackend(ABC):
             gzip_data = BytesIO(gzip_data)
         gzip_data.seek(0)
         with gzip.GzipFile(fileobj=gzip_data, mode="rb") as file:
-            decompressed_data = file.read()
+            try:
+                decompressed_data = file.read()
+            except Exception as e:
+                # Handle decompression errors
+                return gzip_data
         return BytesIO(decompressed_data)
 
     def _compress_torch(self, data):
