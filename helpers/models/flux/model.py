2e392aba46e99e9876ca4deca3dbdc9cb25a1d26
bghira <bghira@users.github.com>
2025-07-09 03:44:46 +0100

    select cond from list if we have just one and let it error otherwise

--- helpers/models/flux/model.py
+++ helpers/models/flux/model.py
@@ -327,6 +327,8 @@ class Flux(ImageModelFoundation):
         if cond is None:
             logger.debug(f"No conditioning latents found :(")
             return batch  # nothing to do
+        if isinstance(cond, (list, tuple)) and len(cond) == 1:
+            cond = cond[0]
 
         packed_cond, cond_ids = build_kontext_inputs(
             cond,
