89af743e635a20b12f652ab4cbbe53841fcfafe2
Bogdan Ghira <59658056+bghira@users.noreply.github.com>
2025-07-09 15:55:23 -0600

    formatting

--- helpers/models/flux/model.py
+++ helpers/models/flux/model.py
@@ -346,7 +346,7 @@ class Flux(ImageModelFoundation):
             cond,
             dtype=self.config.weight_dtype,
             device=self.accelerator.device,
-            latent_channels=self.LATENT_CHANNEL_COUNT
+            latent_channels=self.LATENT_CHANNEL_COUNT,
         )
 
         batch["conditioning_packed_latents"] = packed_cond
