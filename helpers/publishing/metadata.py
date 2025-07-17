d06b5f92a3355ef44c393c6ca8123b6c6f6cddcd
bghira <bghira@users.github.com>
2025-07-17 17:47:36 -0600

    metadata for optimiser config should have spacing

--- helpers/publishing/metadata.py
+++ helpers/publishing/metadata.py
@@ -519,7 +519,7 @@ The text encoder {'**was**' if train_text_encoder else '**was not**'} trained.
   - Number of GPUs: {StateTracker.get_accelerator().num_processes}
 - Gradient checkpointing: {StateTracker.get_args().gradient_checkpointing}
 - Prediction type: {model.PREDICTION_TYPE.value}{model.custom_model_card_schedule_info()}
-- Optimizer: {StateTracker.get_args().optimizer}{optimizer_config if optimizer_config is not None else ''}
+- Optimizer: {StateTracker.get_args().optimizer}{f' (config={optimizer_config})' if optimizer_config is not None else ''}
 - Trainable parameter precision: {'Pure BF16' if torch.backends.mps.is_available() or StateTracker.get_args().mixed_precision == "bf16" else StateTracker.get_args().mixed_precision}
 - Base model precision: `{args.base_model_precision}`
 - Caption dropout probability: {StateTracker.get_args().caption_dropout_probability or 0.0 * 100}%
