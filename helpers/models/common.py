21db65cd6d2f9b66b28669ebf84bcf4c13341920
bghira <bghira@users.github.com>
2025-07-20 16:47:18 +0100

    more unwrap fixes for other locations

--- helpers/models/common.py
+++ helpers/models/common.py
@@ -484,9 +484,9 @@ class ModelFoundation(ABC):
         Moves the model to the target device.
         """
         if self.model is not None:
-            self.model.to(target_device)
+            self.unwrap_model(model=self.model).to(target_device)
         if self.controlnet is not None:
-            self.controlnet.to(target_device)
+            self.unwrap_model(model=self.controlnet).to(target_device)
         if self.vae is not None and self.vae.device != "meta":
             self.vae.to(target_device)
         if self.text_encoders is not None:
@@ -832,7 +832,7 @@ class ModelFoundation(ABC):
                 self.model, "set_gradient_checkpointing_interval"
             ):
                 logger.info("Setting gradient checkpointing interval..")
-                self.model.set_gradient_checkpointing_interval(
+                self.unwrap_model(model=self.model).set_gradient_checkpointing_interval(
                     int(self.config.gradient_checkpointing_interval)
                 )
         self.fuse_qkv_projections()
@@ -911,13 +911,13 @@ class ModelFoundation(ABC):
             setattr(
                 active_pipelines[pipeline_type],
                 self.MODEL_TYPE.value,
-                self.model,
+                self.unwrap_model(model=self.model),
             )
             if self.config.controlnet:
                 setattr(
                     active_pipelines[pipeline_type],
                     "controlnet",
-                    self.controlnet,
+                    self.unwrap_model(model=self.controlnet),
                 )
             return active_pipelines[pipeline_type]
 
