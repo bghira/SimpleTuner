4a28f98f93a470a3f2f57ce15cb59782e2ff2e20
bghira <bghira@users.github.com>
2025-10-06 01:30:32 -0600

    enable info level logging for webui

--- simpletuner/simpletuner_sdk/server/services/webhook_defaults.py
+++ simpletuner/simpletuner_sdk/server/services/webhook_defaults.py
@@ -4,16 +4,15 @@ from __future__ import annotations
 
 import os
 
-
 DEFAULT_CALLBACK_URL = os.environ.get("SIMPLETUNER_WEBHOOK_CALLBACK_URL", "http://localhost:8001/callback")
 
 DEFAULT_WEBHOOK_CONFIG = {
     "webhook_type": "raw",
     "callback_url": DEFAULT_CALLBACK_URL,
+    "log_level": "info",
 }
 
 __all__ = [
     "DEFAULT_CALLBACK_URL",
     "DEFAULT_WEBHOOK_CONFIG",
 ]
-
