2168df54488cb917222d9bc4632504eb52a5e27e
bghira <bghira@users.github.com>
2025-10-07 19:36:19 -0600

    fix test

--- tests/test_webui_e2e.py
+++ tests/test_webui_e2e.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import unittest
 
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
