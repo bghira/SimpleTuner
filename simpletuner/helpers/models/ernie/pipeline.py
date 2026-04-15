f88fdfbec7f8c2999a31728035c28ff91bd22202
bghira <59658056+bghira@users.noreply.github.com>
2026-04-15 00:19:07 -0600

    fix twinflow for ERNIE

--- simpletuner/helpers/models/ernie/pipeline.py
+++ simpletuner/helpers/models/ernie/pipeline.py
@@ -1,6 +1,14 @@
-from diffusers import ErnieImagePipeline as DiffusersErnieImagePipeline
-
 from simpletuner.helpers.models.z_image.pipeline import ZImageLoraLoaderMixin
+
+try:
+    from diffusers import ErnieImagePipeline as DiffusersErnieImagePipeline
+except ImportError as exc:
+    if "ErnieImagePipeline" not in str(exc):
+        raise
+    DiffusersErnieImagePipeline = None
+    _ERNIE_PIPELINE_IMPORT_ERROR = exc
+else:
+    _ERNIE_PIPELINE_IMPORT_ERROR = None
 
 
 class ErnieImageLoraLoaderMixin(ZImageLoraLoaderMixin):
@@ -8,5 +16,23 @@ class ErnieImageLoraLoaderMixin(ZImageLoraLoaderMixin):
     transformer_name = "transformer"
 
 
-class ErnieImagePipeline(ErnieImageLoraLoaderMixin, DiffusersErnieImagePipeline):
-    pass
+if DiffusersErnieImagePipeline is None:
+
+    def _raise_missing_pipeline_import():
+        raise ImportError(
+            "Diffusers does not provide ErnieImagePipeline. Update diffusers to a version "
+            "with ERNIE-Image pipeline support."
+        ) from _ERNIE_PIPELINE_IMPORT_ERROR
+
+    class ErnieImagePipeline(ErnieImageLoraLoaderMixin):
+        def __init__(self, *args, **kwargs):
+            _raise_missing_pipeline_import()
+
+        @classmethod
+        def from_pretrained(cls, *args, **kwargs):
+            _raise_missing_pipeline_import()
+
+else:
+
+    class ErnieImagePipeline(ErnieImageLoraLoaderMixin, DiffusersErnieImagePipeline):
+        pass
